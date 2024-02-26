6eb34d0c71850da0549f179304ed5343be86eb65
Shvam Singh <singhshivam583@yahoo.in>
2024-02-26 23:23:55 +0530

    update

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import { DB_NAME } from './constants.js';
 import connectDB from './db/db.index.js';
 import { app } from './app.js';
 
-dotenv.config({
-    path: '.env',
-})
+// dotenv.config({
+//     path: '.env',
+// })
 
 
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`server is running at port : ${process.env.PORT}`);
+        console.log(`server is running at port: ${process.env.PORT}`);
     })
 })
 .catch((err) => {
